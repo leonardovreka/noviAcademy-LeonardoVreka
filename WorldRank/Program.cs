3941f2da6faad666819fd27dfa706449b4cb4ac2
Ellen Mar <ellenmar@Ellens-MacBook-Pro-2.local>
2026-07-07 12:15:59 +0300

    1st exercise finalgit branch

--- WorldRank/Program.cs
+++ WorldRank/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Player
+{
+    public Guid Id { get; }
+    public string Name { get; set; }
+    public int Score { get; private set; }
+
+    public Player(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Score = 0;
+    }
+}
