24dca8e301ace5cc0c8804b15ae45c49d7432cfd
leonardovreka <leonardovreka@gmail.com>
2026-07-15 22:30:53 +0300

    added player command and  handler

---
+++ Application/Commands/Players/CreatePlayerCommand.Handler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Commands.Players;
+
+public class CreatePlayerCommandHandler : IRequestHandler<CreatePlayerCommand, int>
+{
+    private readonly IPlayerRepository _playerRepository;
+
+    public CreatePlayerCommandHandler(IPlayerRepository playerRepository)
+    {
+        _playerRepository = playerRepository;
+    }
+
+    public async Task<int> Handle(CreatePlayerCommand request, CancellationToken cancellationToken)
+    {
+        var player = new Player(request.Name);
+        player.AddScore(request.Score);
+        await _playerRepository.AddPlayer(player, cancellationToken);
+        return player.Id;
+    }
+}
